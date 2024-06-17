15a17abde1ad992dc0907842249cfe4bf6b65a79
Benoît Allard <benoit.allard@gmx.de>
2024-06-17 17:25:30 +0200

    Fix the fifa script. (floats)

--- fifa_ranking.py
+++ fifa_ranking.py
@@ -12,15 +12,16 @@ MAP = {
     'Bosnia & Herzegovina': 'Bosnia and Herzegovina',
 }
 
+
 class FIFARanking(object):
 
     def __init__(self):
         with open(FILE) as f:
             self.data = list(csv.DictReader(f))
-        self.data.sort(reverse=True, key = lambda d: datetime.date.fromisoformat(d['rank_date']))
+        self.data.sort(reverse=True, key=lambda d: datetime.date.fromisoformat(d['rank_date']))
         self.countries = {}
 
-    def get_ranking(self, country, date = None):
+    def get_ranking(self, country, date=None):
         country = MAP.get(country, country)
         """ Get the ranking of a country at a specific date """
         if date is None:
@@ -28,28 +29,31 @@ class FIFARanking(object):
         elif isinstance(date, str):
             date = datetime.date.fromisoformat(date)
         if country not in self.countries:
-            self.countries[country] = list(filter(lambda d: d['country_full'] == country,  self.data))
+            self.countries[country] = list(filter(lambda d: d['country_full'] == country, self.data))
         for datum in self.countries[country]:
             if datetime.date.fromisoformat(datum['rank_date']) > date:
                 # Skip ranks in the future
                 continue
             # Return the first known rank before the asked date
-            return int(datum['rank'])
+            return int(float(datum['rank']))
         if len(self.countries[country]):
             # We found nothing, yet we have data for the country. Take the oldest entry
-            return int(self.countries[country][-1]['rank'])
+            return int(float(self.countries[country][-1]['rank']))
         print(self.countries[country])
         raise KeyError(country, date)
+
 
 @click.group()
 def cli():
     pass
+
 
 @cli.command('rank')
 @click.argument('country')
 @click.option('--date')
 def echo_ranking(country, date):
     click.echo(f"On {date}, {country} was {FIFARanking().get_ranking(country, date)}")
+
 
 if __name__ == "__main__":
     cli()
